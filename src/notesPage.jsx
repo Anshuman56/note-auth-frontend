ac5845bdc8f7b397f37f12ffb14923d9500714fb
Anshuman56 <anshumandehury@yandex.com>
2026-08-21 10:29:58 +0530

    update: add note deletion

--- src/notesPage.jsx
+++ src/notesPage.jsx
@@ -52,6 +52,20 @@ export default function NotesPase() {
       console.log(err.message);
     }
   }
+
+  async function handleDelet(id) {
+    const warning = window.confirm("Delete this note?");
+    if (warning) {
+      try {
+        const data = await apiFetch(`/notes/${id}`, {
+          method: "delete",
+        });
+        setNotes(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+  }
   return (
     <div className="max-w-2xl mx-auto p-4">
       <div className="flex justify-between items-center w-full mb-5">
@@ -99,6 +113,12 @@ export default function NotesPase() {
                   <span className="text-xs text-gray-500 mt-2">
                     {item.createdAt}
                   </span>
+                  <button
+                    className="text-xs ml-2 cursor-pointer text-red-600 hover:text-red-800"
+                    onClick={() => handleDelet(item._id)}
+                  >
+                    Delete
+                  </button>
                 </li>
               ))}
             </ul>
