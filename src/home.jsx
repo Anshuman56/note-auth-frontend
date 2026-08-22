a04298e92bc17e5c434752f55cd373b29afcc209
Anshuman56 <anshumandehury@yandex.com>
2026-08-22 07:33:46 +0530

    update: change the home page look

--- src/home.jsx
+++ src/home.jsx
@@ -2,19 +2,21 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <>
-      <Link className="p-4 underline" to={"/login"}>
-        Login
-      </Link>
-      <Link className="p-4 underline" to={"/register"}>
-        Register
-      </Link>
-      <h1>
+    <div className="flex flex-col items-center justify-center mt-20 px-4 text-center">
+      <div>
+        <Link className="p-4 underline" to={"/login"}>
+          Login
+        </Link>
+        <Link className="p-4 underline" to={"/register"}>
+          Register
+        </Link>
+      </div>
+      <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight text-slate-900">
         Welcome back{" "}
         <Link className=" underline" to={"/notes"}>
           notes
         </Link>
       </h1>
-    </>
+    </div>
   );
 }
